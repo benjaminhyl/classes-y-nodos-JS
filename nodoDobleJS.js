60c9387dd2e0c26f4b4e21575603592cb532e311
Benjamin Nass <bnass@hyl.cl>
2022-12-01 08:50:14 -0300

    lista doble completa

--- nodoDobleJS.js
+++ nodoDobleJS.js
@@ -9,12 +9,15 @@ class DoubleLinkedList{
     constructor(value){
         this.head = {
             value : value,
-            next : null}
+            next : null,
+            prev : null
+        };
         this.tail = this.head;
         this.length = 1;
     }
     append(value){
         const newNode = new Node(value);
+        newNode.prev = this.tail;
         this.tail.next = newNode;
         this.tail = newNode;
         this.length++;
@@ -23,6 +26,7 @@ class DoubleLinkedList{
     prepend(value){
         const newNode = new Node(value);
         newNode.next = this.head;
+        this.head.prev = newNode;
         this.head = newNode;
         this.length++;
         return this;
@@ -35,8 +39,31 @@ class DoubleLinkedList{
         const indexNode = this.getIndex(index-1);
         const afterIndexPointer = indexNode.next;
         indexNode.next = newNode;
+        afterIndexPointer.prev = newNode;
         newNode.next = afterIndexPointer;
+        newNode.prev = indexNode; 
         this.length++;
+        return this;
+    }
+    remove(index) {
+        if (index >= this.length) {
+            console.log("index fuera de rango");
+            return;
+        }
+        let indexToRemove = this.getTheIndex(index);
+        let nextPointer = indexToRemove.next;
+        let prevPointer = indexToRemove.prev;
+        if (prevPointer && nextPointer) {
+            prevPointer.next = nextPointer;
+            nextPointer.prev = prevPointer;
+        } else if (!prevPointer) {
+            nextPointer.prev = null;
+            this.head = nextPointer;
+        } else if (!nextPointer) {
+            prevPointer.next = null;
+            this.tail = prevPointer;
+        }
+        this.length--;
         return this;
     }
     getIndex(index){
@@ -47,3 +74,4 @@ class DoubleLinkedList{
         return currentNode;
     }
 }
+
