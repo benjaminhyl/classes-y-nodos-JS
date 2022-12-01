3af266144ae6f484a4451906d0b2a4cfb716f315
Benjamin Nass <bnass@hyl.cl>
2022-12-01 11:20:01 -0300

    metodo search 1

---
+++ tree.js
@@ -0,0 +1,45 @@
+class Node{
+    constructor(value){
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+class BinarySearchTree{
+    constructor(){
+        this.root = null;
+    }
+    insert(value){
+        const newNode = new Node(value);
+        if(this.root === null){
+            this.root = newNode;
+        }
+        else {
+            let currentNode = this.root;
+            while(true){
+                if(value < currentNode.value){
+                    if(!currentNode.left){
+                        currentNode.left = newNode;
+                        return this;
+                    }
+                    currentNode = currentNode.left;
+                }
+                else {
+                    if(!currentNode.right){
+                        currentNode.right = newNode;
+                        return this;
+                    }
+                    currentNode = currentNode.right;
+                }
+            }
+        }
+    }
+    search(value){
+        if(this.root === null){
+            return "Tree vacío"
+        }
+        while(true){
+            
+        }
+    }
+}
