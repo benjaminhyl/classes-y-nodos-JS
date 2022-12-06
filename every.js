5ec58bc35518c2f9e587f447574518b9685e6803
Benjamin Nass <bnass@hyl.cl>
2022-12-06 17:15:27 -0300

    mas metodos

---
+++ every.js
@@ -0,0 +1,20 @@
+const team = [
+    {
+      name: "Nicolas",
+      age: 12,
+    },
+    {
+      name: "Andrea",
+      age: 8,
+    },
+    {
+      name: "Zulema",
+      age: 2,
+    },
+    {
+      name: "Santiago",
+      age: 18,
+    },
+  ];
+  //true
+  console.log(team.every((item)=>item.age<20))
---
+++ includes.js
@@ -0,0 +1,26 @@
+const team = [
+    {
+      name: "Nicolas",
+      age: 12,
+    },
+    {
+      name: "Andrea",
+      age: 8,
+    },
+    {
+      name: "Zulema",
+      age: 2,
+    },
+    {
+      name: "Santiago",
+      age: 18,
+    },
+  ];
+  //console.log((team[2].name).includes("Zulema"))
+  //true
+  let arr = ["Osa", "Lupi", "Manchi","ana"];
+  function filtrar(words, query){
+    console.log( words.filter(item => item.includes(query)))
+  }
+  console.log(arr.includes("ana"))
+  filtrar(arr,"a")
