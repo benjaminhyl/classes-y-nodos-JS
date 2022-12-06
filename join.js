8b91d508a11a602c45eb8f3628df0c26f2be5143
Benjamin Nass <bnass@hyl.cl>
2022-12-06 12:03:13 -0300

    guardar metodo con expresiones regulares

--- join.js
+++ join.js
@@ -13,3 +13,5 @@ console.log(elements.join(' ').split(" "));
 // expected output ["Fire", "Air", "Water"]
 let str = "Ejemplo de titulo"
 console.log(str.toLowerCase().split(" ").join("-"));
+let array = [11,1,13,99,8]
+console.log(array.sort((a,b) => a-b))
