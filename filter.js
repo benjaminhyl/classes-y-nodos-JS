531d213be98e8e6a0a4b1f823092168dd5443673
Benjamin Nass <bnass@hyl.cl>
2023-02-17 12:50:05 -0300

    uniq

--- filter.js
+++ filter.js
@@ -4,11 +4,13 @@ function filterByLength(array) {
     )
     return holder;
   }
-let array1 = ["manzana", "mora", "aji"];
-console.log(filterByLength(array1));
+//let array1 = ["manzana", "mora", "aji"];
+//console.log(filterByLength(array1));
 
 //filter an array with another array
-let arr1 = [1,2,3,4];
+let arr1 = [1,2,3,4,7,8];
 let arr2 = [2,4];
-let res = arr1.filter(item => !arr2.includes(item));
-console.log(res);
+//let res = arr1.filter(item => !arr2.includes(item));
+//console.log(res);
+
+console.log( arr1.find(item => item > 5))
