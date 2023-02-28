8ec08715367bd8bdc9c38ba9afce0040c9b14dd2
Benjamin Nass <bnass@hyl.cl>
2023-02-28 13:07:13 -0300

    push

--- uniq.js
+++ uniq.js
@@ -1,28 +1,28 @@
 var a = [1, 1, 2];
 let array = [
-    "7",
-    "7",
-    "6",
-    "5",
-    "5",
-    "5",
-    "5",
-    "5"
- ];
- let obj={
-    a:"1",
-    a:"1",
-b:"2",
-b:"2",
-b:"2",
-b:"3"
- }
- //let b = Object.keys(obj)
- for (key in obj){
-    console.log(obj[key])
- }
+   "7",
+   "7",
+   "6",
+   "5",
+   "5",
+   "5",
+   "5",
+   "5"
+];
+let obj = {
+   a: "1",
+   a: "1",
+   b: "2",
+   b: "2",
+   b: "2",
+   b: "3"
+}
+//let b = Object.keys(obj)
+for (key in obj) {
+   console.log(obj[key])
+}
 let ss = "  Ejemplo     "
-let array_strings = [" hola  ","mundo  ", " nuevo"]
+let array_strings = [" hola  ", "mundo  ", " nuevo"]
 //let b =[... new Set(obj)];
 console.log(ss.trim());
 console.log(ss);
